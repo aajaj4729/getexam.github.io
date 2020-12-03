246704820b31499578db2bae7c98fadff08b84c0
aajaj4729 <74093811+aajaj4729@users.noreply.github.com>
2020-12-03 09:34:36 +0530

    my  first web

---
+++ Online_exam/admin/footer.php
@@ -0,0 +1,4 @@
+<footer>
+	<p class="text-white text-center bg-dark">© Copyright ajconnection. All Rights Reserved
+Designed by Ajaj</p>
+</footer>
---
+++ Online_exam/sublist.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Online Quiz - Quiz List</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="quiz.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+<?php
+include("header.php");
+include("database.php");
+echo "<h1 class='text-center bg-danger'>Select Subject to Give Quiz</h1>";
+$rs=mysqli_query($con,"select * from mst_subject");
+echo "<table class='table table-striped'>";
+while($row=mysqli_fetch_row($rs))
+{
+	echo "<tr><td align=center ><a href=showtest.php?subid=$row[0]><font size=4>$row[1]</font></a>";
+}
+echo "</table>";
+?>
+<?php
+echo '<div class="footer">';
+include("footer.php");
+echo '</div>';
+?>
+</body>
+</html>
